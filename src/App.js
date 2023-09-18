3862f9e4e1083294b39074f0c20e5d6a09c6fd3a
Hussam812 <hossamhosnihussien@gmail.com>
2023-09-18 10:35:26 +0200

    init basic routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
+import { ExerciseDetails, Home, Footer } from "./components";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
+      Navbar
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetails />} />
+      </Routes>
+      <Footer />
+    </Box>
+  );
 };
 
 export default App;
